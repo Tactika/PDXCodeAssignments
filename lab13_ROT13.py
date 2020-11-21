f44624ca0543e0fe3a7ce3d1a5428dc61de20160
Damien Richcreek <richcreekd@live.com>
2020-11-20 19:27:44 -0800

    Updated ROT13 Generator - Removed commented out code

--- lab13_ROT13.py
+++ lab13_ROT13.py
@@ -11,11 +11,10 @@ decoded_lower = [*range(97, 123)]
 decoded_upper = [*range(65, 91)]
 decoded_punct = [*range(33, 65)]
 
+
 def encrypt_data(string, rotations):
     encoded_string = ""
-    #ord_char = ord(string)
     for letter in string:
-        # if letter not in alphabet_lower: 97 / 26 / 97 or 33 /90 / 33
         if letter == " ":
             encoded_string += letter
         elif letter in punct:
@@ -31,10 +30,15 @@ def encrypt_data(string, rotations):
             new_letter = chr(new_letter)
             encoded_string += new_letter
         else:
-            print('Please enter a new string using upper-case, lower-case, and standard punctuation!')
+            print(
+                "Please enter a new string using upper-case, lower-case, and standard punctuation!"
+            )
             string = input("Please enter the string you'd like to encrypt:  ")
-            rotations = int(input("How many rotations would you like to encrypt by?:  "))
-    
+            rotations = int(
+                input("How many rotations would you like to encrypt by?:  ")
+            )
+
     print(encoded_string)
+
 
 encrypt_data(string, rotations)
