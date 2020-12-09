88b7717f7745df5a35c0e7c5049e10cb755fad3f
Damien Richcreek <richcreekd@live.com>
2020-12-08 18:56:36 -0800

    Updated Lab 18 - Peaks and Valleys with PyTest

--- lab18/lab18_peaks_valleys.py
+++ lab18/lab18_peaks_valleys.py
@@ -5,17 +5,17 @@ data_list = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks(data: list) -> list:
     peaks = []
-    for elevation in range(1, (len(data) - 1)):
-        if data[elevation - 1] < data[elevation] > data[elevation + 1]:
-            peaks.append(data.index(data[elevation]))
+    for index in range(1, (len(data) - 1)):
+        if data[index - 1] < data[index] > data[index + 1]:
+            peaks.append(index)
     return peaks
 
 
 def valleys(data: list) -> list:
     valleys = []
-    for elevation in range(1, (len(data) - 1)):
-        if data[elevation - 1] > data[elevation] < data[elevation + 1]:
-            valleys.append(data.index(data[elevation]))
+    for index in range(1, (len(data) - 1)):
+        if data[index - 1] > data[index] < data[index + 1]:
+            valleys.append(index)
     return valleys
 
 
